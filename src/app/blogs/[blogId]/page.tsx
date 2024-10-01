a9141ad7bfb36cf8fc4db80027e19d5b686b655d
meherab nahin <nahinrahman87@gmail.com>
2024-10-01 22:39:38 +0600

    used generateStaticParams for ssg

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,9 +1,15 @@
 import BlogDetails from '@/components/ui/BlogDetails';
+import { TBlogs } from '@/types';
 import React from 'react';
  type TBlogId={
     params:{
         blogId:string
     }
+ }
+export  const generateStaticParams=async()=>{
+    const res=await fetch('http://localhost:5000/blogs')
+    const blog=await res.json()
+    return blog.slice(0,3).map((blog:TBlogs)=>({blogId:blog.id}))
  }
 const BlogDetailsPage =async ({params}:TBlogId) => {
     const res=await fetch(`http://localhost:5000/blogs/${params.blogId}`,{
