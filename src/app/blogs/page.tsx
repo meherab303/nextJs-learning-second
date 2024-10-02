24349b2879cf5709f4b43fd8dce8f0aa91469aa8
meherab nahin <nahinrahman87@gmail.com>
2024-10-02 20:57:43 +0600

    fetching all blogs using rtk query

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,12 +1,17 @@
+
+'use client'
+
 import BlogCard from "@/components/ui/BlogCard";
+import { useGetAllBlogsQuery } from "@/redux/api/baseApi";
 import { TBlogs } from "@/types";
 
 
-const BlogsPage = async() => {
-    const res=await fetch('http://localhost:5000/blogs',{
-        cache:'no-store'
-    })
-    const blogs=await res.json()
+const BlogsPage = () => {
+    // const res=await fetch('http://localhost:5000/blogs',{
+    //     cache:'no-store'
+    // })
+    // const blogs=await res.json()
+    const {data:blogs,isError,isLoading,error}=useGetAllBlogsQuery(undefined)
     return (
         <div className="w-[90%] mx-auto">
              <h1 className="text-center text-3xl my-5">All Blogs from <span className="text-4xl text-accent">Blogiz</span></h1>
